0b5c36b0012423a57c954b6303086a00b8939d4e
AHAMED ABBAS <ahamedali95@gmail.com>
2018-03-21 17:40:25 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,50 @@
+require "pry"
+
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+
+  }
+}
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {};
+
+  languages.each do |programming_style, language_info|
+    language_info.each do |program_name, program_type|
+      new_hash[program_name] = {:type => program_type[:type], :style => []}
+      binding.pry
+      new_hash[program_name][:style].push(programming_style)
+    end
+  end
+
+  #new_hash[:javascript][:style].unshift(:oo)
+  new_hash
 end
+
+reformat_languages(languages)
